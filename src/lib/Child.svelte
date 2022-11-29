7344c52803e1375f99314de2e8aae14e3484347b
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-11-29 12:47:53 +0100

    initial commit

---
+++ src/lib/Child.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let text: string;
+
+  type $$Props = {
+    text: string;
+    "--bg": string;
+  };
+</script>
+
+<p>{text}</p>
+
+<style>
+  p {
+    background-color: var(--bg);
+  }
+</style>
